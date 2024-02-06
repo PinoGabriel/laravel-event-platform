c5f608694b10ab6008ca7c7e2dff70b8b0712078
PinoGabriel <pinogabriel01@gmail.com>
2024-02-06 17:11:03 +0100

    bug: UserSeeder does not exist

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Faker\Guesser\Name;
+use Illuminate\Support\Facades\Hash;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $newUser = new User();
+            $newUser->name = $faker->name();
+            $newUser->email = $faker->email();
+            $newUser->password = Hash::make('password');
+            $newUser->save();
+        }
+    }
+}
