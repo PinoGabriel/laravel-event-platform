4fbbca52f1a08af5682e014d1fc960157696d751
PinoGabriel <pinogabriel01@gmail.com>
2024-02-07 16:28:50 +0100

    first try with api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\EventController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/prova', function () {
+    $utenti = User::all();
+    return response()->json($utenti);
+});
+
+Route::get('/events', [EventController::class, 'index']);
